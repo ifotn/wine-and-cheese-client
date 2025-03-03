fcfd2df071fd3c578036a9281a0ef29fddcc2ae2
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-03-03 15:38:22 -0500

    l8/1 - navbar w/routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CheeseComponent } from './components/cheese/cheese.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: 'cheese', component: CheeseComponent }
+    { path: 'cheese', component: CheeseComponent },
+    { path: '', component: HomeComponent}
 ];
